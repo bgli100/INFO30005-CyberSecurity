20a8d03a46e9eb9b8170db431ada7ea259cdfe9b
Doraxl <xliu16@student.unimelb.edu.au>
2020-05-16 01:03:03 +1000

    User log in page
    First commit user login page

---
+++ hello-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Demo from "./Demo";
+
+function App() {
+  return (
+    <div >
+      <Demo/>
+    </div>
+  );
+}
+
+export default App;
