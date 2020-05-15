1dd3ad0638beea4f83b217fe175e18c7460f61da
Doraxl <xliu16@student.unimelb.edu.au>
2020-05-16 01:07:06 +1000

    Update App.js

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Demo from "./Demo";
+import Login from "./Login";
 
 function App() {
   return (
-    <div >
+    <div>
       <Login/>
     </div>
   );
