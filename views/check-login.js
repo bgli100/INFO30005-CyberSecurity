113b80f37b82f01e63ddbe8b253ce6c9ff47dd91
bgli100 <bgli100@users.noreply.github.com>
2020-06-06 21:32:08 +1000

    responsive nav bar

--- views/check-login.js
+++ views/check-login.js
@@ -3,12 +3,10 @@
       mehtod: "GET",
     }).then((res) => {
       if (res && !res.error) {
-        $("#navbar-main-collapse>ul.d-none>li.logoutonly").hide();
-        $("#navbar-main-collapse>ul.d-none>li.loginonly").show();
-        $("#navbar-main-collapse>ul.mx-auto>li.loginonly").show();
+        $(".logoutonly").hide();
+        $(".loginonly").show();
       } else {
-        $("#navbar-main-collapse>ul.d-none>li.logoutonly").show();
-        $("#navbar-main-collapse>ul.d-none>li.loginonly").hide();
-        $("#navbar-main-collapse>ul.mx-auto>li.loginonly").hide();
+        $(".logoutonly").show();
+        $(".loginonly").hide();
       }
     });
